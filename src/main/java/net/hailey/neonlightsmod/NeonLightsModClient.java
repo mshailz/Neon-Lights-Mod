0e606f337754d52e0af8383aede3184fe3c118ae
Hailey Dolak <haileydolak@yahoo.com>
2022-08-10 15:33:19 -0400

    Initial commit

---
+++ src/main/java/net/hailey/neonlightsmod/NeonLightsModClient.java
@@ -0,0 +1,10 @@
+package net.hailey.neonlightsmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NeonLightsModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}

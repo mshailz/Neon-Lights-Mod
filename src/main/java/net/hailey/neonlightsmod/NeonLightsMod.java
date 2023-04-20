e49a68e626ef2e92ed6748c026ac06a03b5bb394
mshailz <haileydolak@yahoo.com>
2023-04-20 14:14:17 -0400

    Updated code for 1.19.4

--- src/main/java/net/hailey/neonlightsmod/NeonLightsMod.java
+++ src/main/java/net/hailey/neonlightsmod/NeonLightsMod.java
@@ -1,9 +1,10 @@
 package net.hailey.neonlightsmod;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,24 +13,25 @@ public class NeonLightsMod implements ModInitializer {
 	public static final String MOD_ID = "neonlightsmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	public static final ItemGroup ITEM_GROUP = FabricItemGroupBuilder.create(
+	private static final ItemGroup NEON_LIGHTS_MOD = FabricItemGroup.builder(
 			new Identifier("neonlightsmod", "general"))
-			.icon(() -> new ItemStack(ModBlocks.WHITE_NEON_BLOCK))
-			.appendItems(stacks -> {
-				stacks.add(new ItemStack(ModBlocks.NEON_FRAME_BLOCK));
-				stacks.add(new ItemStack(ModBlocks.WHITE_NEON_BLOCK));
-				stacks.add(new ItemStack(ModBlocks.RED_NEON_BLOCK));
-				stacks.add(new ItemStack(ModBlocks.PINK_NEON_BLOCK));
-				stacks.add(new ItemStack(ModBlocks.HOT_PINK_NEON_BLOCK));
-				stacks.add(new ItemStack(ModBlocks.MAGENTA_NEON_BLOCK));
-				stacks.add(new ItemStack(ModBlocks.PURPLE_NEON_BLOCK));
-				stacks.add(new ItemStack(ModBlocks.BLUE_NEON_BLOCK));
-				stacks.add(new ItemStack(ModBlocks.CYAN_NEON_BLOCK));
-				stacks.add(new ItemStack(ModBlocks.LIGHT_BLUE_NEON_BLOCK));
-				stacks.add(new ItemStack(ModBlocks.GREEN_NEON_BLOCK));
-				stacks.add(new ItemStack(ModBlocks.YELLOW_NEON_BLOCK));
-				stacks.add(new ItemStack(ModBlocks.ORANGE_NEON_BLOCK));
+			.displayName(Text.literal("Neon Lights Mod"))
+			.icon(() -> new ItemStack(ModBlocks.CYAN_NEON_BLOCK))
+			.entries((enabledFeatures, entries) -> {
 
+				entries.add(ModBlocks.NEON_FRAME_BLOCK);
+				entries.add(ModBlocks.WHITE_NEON_BLOCK);
+				entries.add(ModBlocks.RED_NEON_BLOCK);
+				entries.add(ModBlocks.PINK_NEON_BLOCK);
+				entries.add(ModBlocks.HOT_PINK_NEON_BLOCK);
+				entries.add(ModBlocks.MAGENTA_NEON_BLOCK);
+				entries.add(ModBlocks.PURPLE_NEON_BLOCK);
+				entries.add(ModBlocks.BLUE_NEON_BLOCK);
+				entries.add(ModBlocks.CYAN_NEON_BLOCK);
+				entries.add(ModBlocks.LIGHT_BLUE_NEON_BLOCK);
+				entries.add(ModBlocks.GREEN_NEON_BLOCK);
+				entries.add(ModBlocks.YELLOW_NEON_BLOCK);
+				entries.add(ModBlocks.ORANGE_NEON_BLOCK);
 			})
 			.build();
 

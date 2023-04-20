84a14d2673efde55bf2edc06efa62b5f73c0741d
mshailz <haileydolak@yahoo.com>
2023-04-20 14:13:36 -0400

    Updated code for 1.19.4

--- src/main/java/net/hailey/neonlightsmod/ModBlocks.java
+++ src/main/java/net/hailey/neonlightsmod/ModBlocks.java
@@ -3,9 +3,10 @@ package net.hailey.neonlightsmod;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 
 public class ModBlocks {
 
@@ -24,18 +25,18 @@ public class ModBlocks {
     public static final Block ORANGE_NEON_BLOCK = new Block(FabricBlockSettings.of(Material.GLASS).strength(0.3f, 1.5f).sounds(BlockSoundGroup.GLASS).luminance(15));
 
     public static void registerBlocks() {
-        Registry.register(Registry.BLOCK, new Identifier(NeonLightsMod.MOD_ID, "neon_frame_block"), NEON_FRAME_BLOCK);
-        Registry.register(Registry.BLOCK, new Identifier(NeonLightsMod.MOD_ID, "white_neon_block"), WHITE_NEON_BLOCK);
-        Registry.register(Registry.BLOCK, new Identifier(NeonLightsMod.MOD_ID, "red_neon_block"), RED_NEON_BLOCK);
-        Registry.register(Registry.BLOCK, new Identifier(NeonLightsMod.MOD_ID, "pink_neon_block"), PINK_NEON_BLOCK);
-        Registry.register(Registry.BLOCK, new Identifier(NeonLightsMod.MOD_ID, "hot_pink_neon_block"), HOT_PINK_NEON_BLOCK);
-        Registry.register(Registry.BLOCK, new Identifier(NeonLightsMod.MOD_ID, "magenta_neon_block"), MAGENTA_NEON_BLOCK);
-        Registry.register(Registry.BLOCK, new Identifier(NeonLightsMod.MOD_ID, "purple_neon_block"), PURPLE_NEON_BLOCK);
-        Registry.register(Registry.BLOCK, new Identifier(NeonLightsMod.MOD_ID, "blue_neon_block"), BLUE_NEON_BLOCK);
-        Registry.register(Registry.BLOCK, new Identifier(NeonLightsMod.MOD_ID, "cyan_neon_block"), CYAN_NEON_BLOCK);
-        Registry.register(Registry.BLOCK, new Identifier(NeonLightsMod.MOD_ID, "light_blue_neon_block"), LIGHT_BLUE_NEON_BLOCK);
-        Registry.register(Registry.BLOCK, new Identifier(NeonLightsMod.MOD_ID, "green_neon_block"), GREEN_NEON_BLOCK);
-        Registry.register(Registry.BLOCK, new Identifier(NeonLightsMod.MOD_ID, "yellow_neon_block"), YELLOW_NEON_BLOCK);
-        Registry.register(Registry.BLOCK, new Identifier(NeonLightsMod.MOD_ID, "orange_neon_block"), ORANGE_NEON_BLOCK);
+        Registry.register(Registries.BLOCK, new Identifier(NeonLightsMod.MOD_ID, "neon_frame_block"), NEON_FRAME_BLOCK);
+        Registry.register(Registries.BLOCK, new Identifier(NeonLightsMod.MOD_ID, "white_neon_block"), WHITE_NEON_BLOCK);
+        Registry.register(Registries.BLOCK, new Identifier(NeonLightsMod.MOD_ID, "red_neon_block"), RED_NEON_BLOCK);
+        Registry.register(Registries.BLOCK, new Identifier(NeonLightsMod.MOD_ID, "pink_neon_block"), PINK_NEON_BLOCK);
+        Registry.register(Registries.BLOCK, new Identifier(NeonLightsMod.MOD_ID, "hot_pink_neon_block"), HOT_PINK_NEON_BLOCK);
+        Registry.register(Registries.BLOCK, new Identifier(NeonLightsMod.MOD_ID, "magenta_neon_block"), MAGENTA_NEON_BLOCK);
+        Registry.register(Registries.BLOCK, new Identifier(NeonLightsMod.MOD_ID, "purple_neon_block"), PURPLE_NEON_BLOCK);
+        Registry.register(Registries.BLOCK, new Identifier(NeonLightsMod.MOD_ID, "blue_neon_block"), BLUE_NEON_BLOCK);
+        Registry.register(Registries.BLOCK, new Identifier(NeonLightsMod.MOD_ID, "cyan_neon_block"), CYAN_NEON_BLOCK);
+        Registry.register(Registries.BLOCK, new Identifier(NeonLightsMod.MOD_ID, "light_blue_neon_block"), LIGHT_BLUE_NEON_BLOCK);
+        Registry.register(Registries.BLOCK, new Identifier(NeonLightsMod.MOD_ID, "green_neon_block"), GREEN_NEON_BLOCK);
+        Registry.register(Registries.BLOCK, new Identifier(NeonLightsMod.MOD_ID, "yellow_neon_block"), YELLOW_NEON_BLOCK);
+        Registry.register(Registries.BLOCK, new Identifier(NeonLightsMod.MOD_ID, "orange_neon_block"), ORANGE_NEON_BLOCK);
     }
 }
